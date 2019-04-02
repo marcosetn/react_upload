2fd2254218dea392203bd241bd4716afae2518ba
marc <marc@bdiworks.com>
2019-04-02 19:30:46 +0100

    pseudo upload with timer

--- src/actions/file_upload.js
+++ src/actions/file_upload.js
@@ -17,8 +17,92 @@ const ADMIN_URL = _URL + 'admin/';
 
 const VALID_UPLOAD=Object.freeze({"Uploadable":0, "Cancelled":1, "Too Big":2, "Wrong Filetype":3});
 
+const sleep = (milliseconds) => {
+	return new Promise(resolve => setTimeout(resolve, milliseconds))
+  };
+
+
 
 const upload_list = (list, index, dir, filetype, dispatch)=>{
+    return new Promise((resolve, reject)=>{        
+        function upload_file(list, index, dir, filetype, dispatch)
+        {
+            return new Promise((resolve)=>
+            {
+                const item = list[index];
+                if(item.enabled !== VALID_UPLOAD.Uploadable)
+                    return resolve(++index);
+                let f = new FormData();        
+                f.append('items[]', JSON.stringify({fname:item.fname, file_data:item.file_data}));
+                f.append('dir', dir);
+				f.append('filetype', filetype);
+
+
+				let inner_index = 0;
+				window.setTimeout(()=>{
+				while(inner_index <= 10)
+				{
+					console.log(index, inner_index);
+					if(inner_index === 10)
+					{
+						dispatch({type:FILE_PROGRESS, complete:100, fname:item.fname});
+						index++;
+						return resolve(index);
+					}
+					else
+					{
+						inner_index++;						
+						dispatch({type:FILE_PROGRESS, complete:inner_index*10, fname:item.fname});
+					}
+				}
+			}, 1000);
+
+
+				/*
+                const xhr = new XMLHttpRequest();
+
+                xhr.addEventListener("progress", function(e) {
+                    if (e.lengthComputable) {
+                        let percentComplete = e.loaded / e.total * 100;
+                        dispatch({type:FILE_PROGRESS, complete:e.loaded/e.total, fname:item.fname});
+                    }
+                });
+
+                xhr.addEventListener('load', (e)=>{
+                    const obj = JSON.parse(e.currentTarget.response);
+                    if(!obj.hasOwnProperty('error'))
+                        dispatch({type:FILE_PROGRESS, complete:100, fname:item.fname});
+                    else
+                        dispatch({type:FILE_PROGRESS, complete:obj.error, fname:item.fname});
+                    index++;
+                    return resolve(index);
+                });
+                xhr.addEventListener('error', e=>{
+                    console.dir(e);
+                });
+                xhr.open("POST", _URL+'do_upload');
+				xhr.send(f);
+				*/
+            });
+        }
+        function recursive_loop(list, index, dir, filetype, dispatch)
+        {
+            upload_file(list, index, dir, filetype, dispatch)
+            .then(val=>{index=val;
+                if(index<list.length)
+                    recursive_loop(list, index, dir, filetype, dispatch)
+                else
+                    resolve('all done');
+            });
+        }
+        return recursive_loop(list, index, dir, filetype, dispatch);
+    });
+}
+
+
+
+
+const real_upload_list = (list, index, dir, filetype, dispatch)=>{
     return new Promise((resolve, reject)=>{        
         function upload_file(list, index, dir, filetype, dispatch)
         {
