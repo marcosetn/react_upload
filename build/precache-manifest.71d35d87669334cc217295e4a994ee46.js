6903b2e5ccc77594cde10ddb32deac395f35c5a0
marc <marc@bdiworks.com>
2019-04-03 21:09:45 +0100

    add build to upload for github

---
+++ build/precache-manifest.71d35d87669334cc217295e4a994ee46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4700deb62dd37afb697",
+    "url": "/react_upload/build/static/js/runtime~main.dd66326b.js"
+  },
+  {
+    "revision": "e072474ff6dd5201a27d",
+    "url": "/react_upload/build/static/js/main.9e0c6fda.chunk.js"
+  },
+  {
+    "revision": "038781c61e62a1ebc931",
+    "url": "/react_upload/build/static/js/2.206fcc53.chunk.js"
+  },
+  {
+    "revision": "e072474ff6dd5201a27d",
+    "url": "/react_upload/build/static/css/main.80076b4a.chunk.css"
+  },
+  {
+    "revision": "1115de8fb044b21c1e907e4d42a4d659",
+    "url": "/react_upload/build/index.html"
+  }
+];
